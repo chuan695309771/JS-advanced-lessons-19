2b9a22935d437c653277486e0d72647ef10dd978
chuan695309771 <695309771@qq.com>
2019-05-05 09:32:28 +0800

    正则

---
+++ 课程练习/20-RegExp正则表达式/demo01.js
@@ -0,0 +1,9 @@
+var reg1 = /[bcf]at/gi;
+
+var reg2 = new RegExp(/[bcf]at/,"gi");
+var reg3 = new RegExp("[bcf]at","gi");
+
+console.log("a fAt bat ,a faT cat".match(reg1));
+
+var str1 = "fat Fat bat cat jkdfdj llo";
+console.log(str1.match(reg1));
